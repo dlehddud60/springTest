942cf4fbceeea94c3123c15194debaf669822aa7
dlehddud60 <dlehddud60@naver.com>
2023-03-14 01:26:41 +0900

    Junit 테스트실습과 repository 생성

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -12,9 +14,35 @@ public class HelloController {
         return "hello";
     }
 
-    @GetMapping("test")
-    public String test(Model model){
-        model.addAttribute("test","test입니다.");
-        return "hello";
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello-template";
     }
+
+    @GetMapping("hello-string")
+    @ResponseBody//http의 바디부분의 이 데이터를 직접 넣어주겠다는 의미이다.
+    public String helloString(@RequestParam("name")String name){
+        return "hello" + name; //hello spring 랜더링 없이 전송파라미터로 준 문자만 출력한다.
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
 }
