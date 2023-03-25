e225bec07ce2d9c7a947a90d26074d5f0bc89960
dlehddud60 <dlehddud60@naver.com>
2023-03-25 20:34:36 +0900

    새로운 프로젝트생성

---
+++ core1/src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,28 @@
+package hello.core.member;
+
+import hello.core.Appconfig;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach() {
+        Appconfig appconfig = new Appconfig();
+        memberService = appconfig.memberService();
+    }
+
+    @Test
+    void join() {
+        //given 이런이런것이 주어졌을 때
+        Member member = new Member(1L,"memberA",Grade.VIP);
+        //when 이렇게 했을 때
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+        //then 이렇게 된다.
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
+}
---
+++ core1/src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,33 @@
+package hello.core.order;
+
+import hello.core.Appconfig;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.*;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    MemberService memberService;
+    OrderService orderService;
+
+    @BeforeEach
+    public void beforeEach() {
+        Appconfig appconfig = new Appconfig();
+        memberService = appconfig.memberService();
+        orderService = appconfig.orderService();
+    }
+
+    @Test
+    void createOrder() {
+       Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+
+    }
+
+}
